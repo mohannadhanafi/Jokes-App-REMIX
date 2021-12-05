8fd68db5dd1e71ae1aeaa26ecd4458d50f2ddca0
mohannadhanafi <mohannad.hanafi96@gmail.com>
2021-12-05 21:15:45 +0200

    Featuer - Made page of one joke

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,24 @@
+import { LoaderFunction, useLoaderData, Link } from "remix";
+import type { Joke } from "@prisma/client";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { joke: Joke };
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const joke = await db.joke.findUnique({ where: { id: params.jokeId } });
+  if (joke) {
+    return joke;
+  } else throw new Error("Joke not found");
+};
+
 const JokeRoute = () => {
+  const data = useLoaderData<LoaderData>();
+  console.log({data})
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{data.content}</p>
+      <Link to=".">{data.name} Permalink</Link>
     </div>
   );
 };
